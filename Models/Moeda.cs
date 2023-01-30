c5d0df5f7d0a454647a3fe8764d788b01f9b82aa
Lucas Tavares <lucastavasousa@gmail.com>
2023-01-30 19:41:13 -0300

    Adicionando novas atributos nos modelos

--- Models/Moeda.cs
+++ Models/Moeda.cs
@@ -1,9 +1,12 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MvcMoeda.Models;
 
 public class Moeda
 {
+    [Key]
     public int Id { get; set; }
     public string Nome { get; set; }
 
--- Models/Monitoramento.cs
+++ Models/Monitoramento.cs
@@ -1,4 +1,7 @@
 
+using System.ComponentModel.DataAnnotations.Schema;
+using MvcMoeda.Models;
+
 namespace MvcMonitoramento.Models;
 
 public class Monitoramento
@@ -6,20 +9,22 @@ public class Monitoramento
     public int Id { get; set; }
     public string Nome { get; set; }
 
+
+
+    public int MoedaId1 { get; set; }
+    [ForeignKey("MoedaId1")]
+    public Moeda Moeda1 { get; set;}
+
+    public int MoedaId2 { get; set; }
+    [ForeignKey("MoedaId2")]
+    public Moeda Moeda2 { get; set;}
+
     
-
-    // [ForeignKey("Moeda")]
-    // public int MoedaId1 { get; set; }
-    // public Moeda Moeda1 { get; set;}
-
     
-    
-    // [ForeignKey("Moeda")]
-    // public int MoedaId2 { get; set; }
-    // public Moeda Moeda2 { get; set;}
+   
 
-    // public float ValorDeVenda {get ; set;}
+    public float ValorDeVenda {get ; set;}
 
-    // public float ValorDeCompra {get; set;}
+    public float ValorDeCompra {get; set;}
 
 }
